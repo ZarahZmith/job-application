b5503cc4190ff7749d2a084af0aff2b1262c44e4
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-08 14:49:48 -0500

    Created blur event for full name and logged out full name content and length

--- src/UI.js
+++ src/UI.js
@@ -1,3 +1,16 @@
-(function testFun() {
+(function jobAppModule() {
+  //tests
   console.log("hello!");
+  document.querySelector('.btn-warning').addEventListener('click', function printHello(event) {
+    console.log("JavaScript is challenging!");
+    console.log(event);
+  } );
+
+  document.querySelector('#full-name').addEventListener('blur', function fullName(evt) {
+    //test
+    console.log(evt);
+    
+    console.log(evt.target.value);
+    console.log(evt.target.value.length);
+  });
 })();
