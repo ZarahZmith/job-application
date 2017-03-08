f2c8eec52d014ba2d6aa06896a6e36910b5b93cf
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-08 18:13:46 -0500

    Set languange checkboxes to adjust to number upon being checked; prevented sumbmission while also allowing data to appear in console upon being entered in the form in the browser

--- src/UI.js
+++ src/UI.js
@@ -1,10 +1,14 @@
 (function jobAppModule() {
+  window.applyApp = window.applyApp || {};
+
   //tests
-  console.log("hello!");
-  document.querySelector('.btn-warning').addEventListener('click', function printHello(evt) {
-    console.log("JavaScript is challenging!");
-    console.log(evt);
-  } );
+  // console.log("hello!");
+  // document.querySelector('.btn-warning').addEventListener('click', function printHello(evt) {
+  //   console.log("JavaScript is challenging!");
+  //   console.log(evt);
+  // } );
+
+
 
   document.querySelector('#full-name').addEventListener('blur', function fullName(evt) {
     //tests
@@ -20,9 +24,39 @@
   });
 
   document.querySelector('#exp').addEventListener('change', function updateExp(evt) {
-    console.log(evt.target.value);
-    console.log(evt.target.parentNode.childNodes[3]);
+    //tests
+    // console.log(evt.target.value);
+    //console.log(evt.target.parentNode.childNodes[3]);
+
     evt.target.parentNode.childNodes[3].innerText = evt.target.value + ' years';
+  });
+
+  let count = 0;
+
+  Array.from(document.querySelectorAll('[name="languages"]'))
+    .forEach(function addCheckClick(item) {
+      item.addEventListener('change', function updateLangs(evt) {
+        //test
+        // console.log(evt.target.checked);
+        if (evt.target.checked) {
+          count++;
+        } else {
+          count--;
+        }
+        // console.log(evt.target.parentNode.parentNode.childNodes);
+        evt.target.parentNode.parentNode.childNodes[7].innerText = count + ' languages';
+      });
+    });
+
+  document.querySelector('form').addEventListener('submit', function applyFn(evt) {
+    evt.preventDefault();
+    console.log('ayyyyye', evt);
+    window.applyApp.submitApp({
+      fullName : document.querySelector('#full-name').value,
+      experience: document.querySelector('#exp').value,
+      languages: document.querySelector('[name="languages"]').value,
+      prevWork: document.querySelector('[name="work-url"]').value
+    });
   });
 
 })();
--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
-(function testFun() {
+(function jobAppModule() {
+  window.applyApp = window.applyApp || {};
+
   console.log("hello! from data");
+
+  window.applyApp.submitApp = function sumbitApp(data) {
+    console.log("ayeeeee data", data);
+  };
+
 })();
