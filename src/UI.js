f6a25ba6bdf298bb38901be1fc006591c22eea45
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-08 15:05:59 -0500

    Error notification occurs for full name if nothing is typed, but does not occur if even one character is typed

--- src/UI.js
+++ src/UI.js
@@ -9,8 +9,13 @@
   document.querySelector('#full-name').addEventListener('blur', function fullName(evt) {
     //test
     console.log(evt);
-    
     console.log(evt.target.value);
-    console.log(evt.target.value.length);
+
+    if (evt.target.value.length) {
+      evt.target.parentNode.parentNode.classList.remove('has-error');
+    } else {
+      evt.target.parentNode.parentNode.classList.add('has-error');
+    }
+
   });
 })();
